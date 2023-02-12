7912cb01f09db5b32f6dd296ee9c0a3f6739d66a
quangnguyen1399 <nhatquangnguyen1399@gmail.com>
2023-02-12 15:16:11 +0700

    get browserDriver

---
+++ testcases/com/wp/posts/Level_01_Login_Repeat_Yourself.java
@@ -0,0 +1,50 @@
+package com.wp.posts;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+
+public class Level_01_Login_Repeat_Yourself {
+	WebDriver driver;
+	String projectLocation = System.getProperty("user.dir");
+	
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.chrome.driver", projectLocation + "\\browserDriver\\chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+	
+	@BeforeMethod
+	public void beforeMethod() {
+		driver.get("https://www.facebook.com/");
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void TC_01_New_Post() {
+	}
+
+	@Test
+	public void TC_02_View_Post() {
+	}
+
+	@Test
+	public void TC_03_Edit_Post() {
+	}
+
+	@Test
+	public void TC_04_Make_Post_Private() {
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+}
